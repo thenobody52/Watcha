aea7b07e1477d0a3c0d7a4e8d2475e8f6ec1dd99
Nobody <thenobody52@gmail.com>
2026-07-27 20:27:14 +0600

    fix: remove hardcoded debug keystore requirement for CI stability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug { }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
